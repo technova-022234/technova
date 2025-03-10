53c7d2d3be18b5c6b4aa02b78b3a7115011df7b6
technova-022234 <technova022234@gmail.com>
2025-03-10 10:52:22 +0530

    add localstorage handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from "react-router-dom";
 import Loginpage from "./pages/Loginpage";
 import SpaceshipConsole from "./pages/Level1";
 import Puzzle from "./pages/Level2";
@@ -24,8 +29,13 @@ const App = () => {
     const [user, setUser] = useState(localStorage.getItem("userEmail"));
 
     // Redux selectors for progress
-    const level1Completed = useSelector((state) => state.progress.level2);
-    const level2Completed = useSelector((state) => state.progress.level3);
+    const level1Completed =
+        useSelector((state) => state.progress.level2) ||
+        localStorage.getItem("levelComplete") === "true";
+    const level2Completed =
+        useSelector((state) => state.progress.level3) ||
+        localStorage.getItem("puzzleSolved") === "true";
+
     console.log(level1Completed);
     console.log(level2Completed);
     console.log(user);
@@ -33,35 +43,76 @@ const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/instructions" element={<InstructionsPageLevel1 />} />
-                <Route path="/level2instructions" element={<InstructionsPageLevel2 />} />
-                <Route path="/level3instructions" element={<InstructionsPageLevel3 />} />
+                <Route
+                    path="/instructions"
+                    element={<InstructionsPageLevel1 />}
+                />
+                <Route
+                    path="/level2instructions"
+                    element={<InstructionsPageLevel2 />}
+                />
+                <Route
+                    path="/level3instructions"
+                    element={<InstructionsPageLevel3 />}
+                />
                 <Route path="/" element={<Loadingpage />} />
                 <Route
                     path="/login"
                     // Pass setUser to your Loginpage component so it can update the state after a successful login
-                    element={!user ? <Loginpage setUser={setUser} /> : <Navigate to="/nav" />}
+                    element={
+                        !user ? (
+                            <Loginpage setUser={setUser} />
+                        ) : (
+                            <Navigate to="/nav" />
+                        )
+                    }
                 />
                 <Route path="/nav" element={<NavPage />} />
                 <Route
                     path="/story"
-                    element={user ? <CinematicSequence /> : <Navigate to="/login" />}
+                    element={
+                        user ? <CinematicSequence /> : <Navigate to="/login" />
+                    }
                 />
                 <Route
                     path="/level1story"
-                    element={level1Completed ? <Level1Story /> : <Navigate to="/level1" />}
+                    element={
+                        level1Completed ? (
+                            <Level1Story />
+                        ) : (
+                            <Navigate to="/level1" />
+                        )
+                    }
                 />
                 <Route
                     path="/level2story"
-                    element={level1Completed ? <Level2Story /> : <Navigate to="/level2" />}
+                    element={
+                        level2Completed ? (
+                            <Level2Story />
+                        ) : (
+                            <Navigate to="/level2" />
+                        )
+                    }
                 />
                 <Route
                     path="/level2storycontinued"
-                    element={level2Completed ? <Level2StoryContinued /> : <Navigate to="/level2" />}
+                    element={
+                        level2Completed ? (
+                            <Level2StoryContinued />
+                        ) : (
+                            <Navigate to="/level2" />
+                        )
+                    }
                 />
                 <Route path="/leaderboard_level1" element={<Leaderboard />} />
-                <Route path="/leaderboard_level2" element={<Level2Leaderboard />} />
-                <Route path="/leaderboard_level3" element={<Level3Leaderboard />} />
+                <Route
+                    path="/leaderboard_level2"
+                    element={<Level2Leaderboard />}
+                />
+                <Route
+                    path="/leaderboard_level3"
+                    element={<Level3Leaderboard />}
+                />
                 <Route
                     path="/*"
                     element={
@@ -70,15 +121,33 @@ const App = () => {
                             <Routes>
                                 <Route
                                     path="/level1"
-                                    element={user ? <SpaceshipConsole /> : <Navigate to="/login" />}
+                                    element={
+                                        user ? (
+                                            <SpaceshipConsole />
+                                        ) : (
+                                            <Navigate to="/login" />
+                                        )
+                                    }
                                 />
                                 <Route
                                     path="/level2"
-                                    element={level1Completed ? <Puzzle /> : <Navigate to="/level1" />}
+                                    element={
+                                        level1Completed ? (
+                                            <Puzzle />
+                                        ) : (
+                                            <Navigate to="/level1" />
+                                        )
+                                    }
                                 />
                                 <Route
                                     path="/level3"
-                                    element={level2Completed ? <Level3 /> : <Navigate to="/level2" />}
+                                    element={
+                                        level2Completed ? (
+                                            <Level3 />
+                                        ) : (
+                                            <Navigate to="/level2" />
+                                        )
+                                    }
                                 />
                             </Routes>
                         </>
